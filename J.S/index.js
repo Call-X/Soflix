5f01518bd728ec65b8842d9c7857f46ce5fffba8
Call-X <emile.miath.architecte@hotmail.com>
2021-12-28 17:53:28 +0100

    FC 28122021

--- J.S/index.js
+++ J.S/index.js
@@ -5,11 +5,13 @@ const carousel = document.getElementById("carousel"),
   next = document.getElementById("next"),
   prev = document.getElementById("prev");
 
+
 next.addEventListener("click", e => {
   carousel.scrollBy(width + gap, 0);
   if (carousel.scrollWidth !== 0) {
     prev.style.display = "flex";
   }
+
   if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
     next.style.display = "none";
   }
@@ -26,3 +28,41 @@ prev.addEventListener("click", e => {
 
 let width = carousel.offsetWidth;
 window.addEventListener("resize", e => (width = carousel.offsetWidth));
+
+
+const searchInput = document.getElementById('search');
+const results = document.getElementById('results');
+
+let movies ;
+let searchTerm = '';
+
+// API REQUEST
+const fetchMovies = async() => {
+    movies = await fetch(
+        'http://localhost:8000/api/v1/titles?genre_contains=fantasy').then(res => 
+        res.json());
+};
+
+const showMovies = async() => {
+    await fetchMovies();
+
+    results.innerHTML = (
+        movies
+            .filters(movie => movie.title.toLowerCase().includes(searchTerm.toLowerCase() 
+            ))
+            .map(movie => (
+
+                `
+                  <li class="movie-item">
+                    <img class="movie-flag" src="${movie.flag}" />
+                    <h3 class="movie-title">${movie.title}</h3>
+                    <div class="movie-info">
+                        <h2 class="movie-category">${movie.category}
+                    </div>    
+                  </li>
+                `
+
+
+            )).join('')
+    );
+};
